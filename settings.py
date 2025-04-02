99762614a28a988254202557b491204ffcf9b279
Ramón Doval Dev <ramondoval79@gmail.com>
2025-04-02 20:50:08 +0200

    Drop alien flee at the edge of the screen and change direction

--- settings.py
+++ settings.py
@@ -16,3 +16,9 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
+
+        # Alien configuration
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction = 1 represents right; -1 represents left
+        self.fleet_direction = 1
